f3776ea7a31f6decee5bd566016b29d35b0bb8c4
Christian Ray Leovido <christian.leovido@gmail.com>
2022-01-19 15:10:34 +0000

    Initial Commit

---
+++ Domain-Driven-MVVM/Domain_Driven_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  Domain_Driven_MVVMApp.swift
+//  Domain-Driven-MVVM
+//
+//  Created by Christian Leovido on 19/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct Domain_Driven_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
